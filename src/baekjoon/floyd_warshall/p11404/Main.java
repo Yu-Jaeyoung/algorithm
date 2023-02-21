a0ea1932410b8acc2106081adc7cbc3c576abbd8
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-02-21 23:59:03 +0900

    resolved problem / 배열 초기화 간 값 추가 진행

--- src/baekjoon/floyd_warshall/p11404/Main.java
+++ src/baekjoon/floyd_warshall/p11404/Main.java
@@ -1,6 +1,25 @@
 package src.baekjoon.floyd_warshall.p11404;
 
 public class Main {
+    public static void main(String[] args) {
+        Floyd floyd = new Floyd(5);
+        floyd.firstAddEdge(5);
+        floyd.addEdge(1,2,2);
+        floyd.addEdge(1,3,3);
+        floyd.addEdge(1,4,1);
+        floyd.addEdge(1,5,10);
+        floyd.addEdge(2,4,2);
+        floyd.addEdge(3,4,1);
+        floyd.addEdge(3,5,1);
+        floyd.addEdge(4,5,3);
+        floyd.addEdge(3,5,10);
+        floyd.addEdge(3,1,8);
+        floyd.addEdge(1,4,2);
+        floyd.addEdge(5,1,7);
+        floyd.addEdge(3,4,2);
+        floyd.addEdge(5,2,4);
+        floyd.floyd(5);
+    }
 
 }
 
@@ -12,8 +31,26 @@ class Floyd {
         graph = new int[num][num];
     }
 
+
+    public void firstAddEdge(int num) {
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < num; j++) {
+                graph[i][j] = 100001;
+                if (i == j) {
+                    graph[i][j] = 0;
+                }
+            }
+        }
+    }
+
     public void addEdge(int start, int end, int weight) {
-        graph[start - 1][end - 1] = weight;
+        if (graph[start - 1][end - 1] == 0) {
+            graph[start - 1][end - 1] = weight;
+        } else if (graph[start - 1][end - 1] > weight) {
+            graph[start - 1][end - 1] = weight;
+        } else {
+            return;
+        }
     }
 
     public void printGraph(int maxNode) {
