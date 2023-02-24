28b1e0fd643b3855d322e79c9e889bbb12b419cb
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-02-24 19:18:45 +0900

    코드 작성 스타일 변경 필요!!

--- src/baekjoon/floyd_warshall/p11404/Main.java
+++ src/baekjoon/floyd_warshall/p11404/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
         int maxVertexNum = Integer.parseInt(bufferedReader.readLine());
         Floyd floyd = new Floyd(maxVertexNum);
-        floyd.makeGraph(maxVertexNum);
+        floyd.makeGraph();
 
         int edgeCount = Integer.parseInt(bufferedReader.readLine());
 
@@ -25,23 +25,24 @@ public class Main {
             floyd.addEdge(start, end, weight);
         }
 
-        floyd.floyd(maxVertexNum);
+        floyd.floyd(maxVertexNum);  // 이런식으로 플로이드->플로이드 가 아닌,
+                                    // 플로이드 -> run() or 그래프 -> 플로이드 식으로 작성 진행
     }
 
 }
 
-class Floyd {
+class Floyd { // 무엇을 클래스로 할 것인가에 대한 고민도 필요
 
     private int[][] graph;
 
-    Floyd(int maxVertexNum) {
+    Floyd(final int maxVertexNum) {
         graph = new int[maxVertexNum][maxVertexNum];
     }
 
 
-    public void makeGraph(int maxVertexNum) {
-        for (int i = 0; i < maxVertexNum; i++) {
-            for (int j = 0; j < maxVertexNum; j++) {
+    public void makeGraph() {
+        for (int i = 0; i < graph.length; i++) { // 배열을 활용하므로 향상된 for문으로 변경할 수 있음
+            for (int j = 0; j < graph.length; j++) {
                 graph[i][j] = 100001;
                 if (i == j) {
                     graph[i][j] = 0;
@@ -50,14 +51,11 @@ class Floyd {
         }
     }
 
-    public void addEdge(int start, int end, int weight) {
-        if (graph[start - 1][end - 1] == 0) {
-            graph[start - 1][end - 1] = weight;
-        } else if (graph[start - 1][end - 1] > weight) {
-            graph[start - 1][end - 1] = weight;
-        } else {
+    public void addEdge(int start, int end, int weight) { // 예외 조건을 처리 한 후 작성하는 방법이 가독성이 좋음
+        if (graph[start - 1][end - 1] < weight) {
             return;
         }
+        graph[start - 1][end - 1] = weight;
     }
 
     public void printGraph(int maxVertexNum) {
