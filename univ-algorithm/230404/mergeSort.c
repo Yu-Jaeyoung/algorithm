b2d41dc358e96d6133ef9a7b61565e105ae44166
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-04-09 20:59:09 +0900

    analyzing mergeSort example

--- univ-algorithm/230404/mergeSort.c
+++ univ-algorithm/230404/mergeSort.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 
+int count = 0;
+
 void merge(int data[], int left, int middle, int right) {
 
     int *tmp = (int *) malloc(sizeof(int) * (right - left + 1));
@@ -11,11 +13,19 @@ void merge(int data[], int left, int middle, int right) {
     j = middle + 1;
     k = left;
 
+    printf("merge 호출 횟수 : %d\n", ++count);
+    printf("할당된 공간의 크기 : %d\n", right - left + 1);
+
     while (i <= middle && j <= right) {
+        printf("비교 대상 값 1번 : %d 2번 : %d\n", data[i], data[j]);
         if (data[i] <= data[j]) {
+            printf("1번 출력 / 접근하는 인덱스 번호 : %d\n", k);
             tmp[k++] = data[i++];
+            printf("2번 출력 / 접근하는 인덱스 번호의 저장 후 값 : %d\n",tmp[k - 1]);
         } else {
+            printf("3번 출력 / 접근하는 인덱스 번호 : %d\n", k);
             tmp[k++] = data[j++];
+            printf("4번 출력 / 접근하는 인덱스 번호의 저장 후 값 : %d\n",tmp[k - 1]);
         }
     }
 
@@ -30,9 +40,10 @@ void merge(int data[], int left, int middle, int right) {
     }
 
     for (l = left; l <= right; l++) {
+        printf("tmp[l]의 값 : %d\n", tmp[l]);
         data[l] = tmp[l];
     }
-
+    printf("\n");
     free(tmp);
 }
 
@@ -68,7 +79,7 @@ int main() {
         printf("%d ", data[i]);
     }
 
-    printf("\n정렬 후 배열 : ");
+    printf("\n정렬 후 배열 : \n");
 
     mergeSort(data, 0, n - 1);
 
