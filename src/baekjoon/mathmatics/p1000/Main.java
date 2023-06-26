afd0c384c1cd75df980aea8a56e4a9548a44b672
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-06-26 22:35:04 +0900

    #1000 / mathmatics / A + B

---
+++ src/baekjoon/mathmatics/p1000/Main.java
@@ -0,0 +1,18 @@
+package src.baekjoon.mathmatics.p1000;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        String readLine = bufferedReader.readLine();
+        StringTokenizer token = new StringTokenizer(readLine);
+        int token_1 = Integer.parseInt(token.nextToken());
+        int token_2 = Integer.parseInt(token.nextToken());
+        System.out.print(token_1 + token_2);
+    }
+}
