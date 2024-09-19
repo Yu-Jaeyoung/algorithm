03bee40a7d6deb73bcde172826c99f2293b63520
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2024-09-19 14:44:38 +0900

    Insertion Sort

--- univ-algorithm/java/sorting/Main.java
+++ univ-algorithm/java/sorting/Main.java
@@ -1,4 +1,5 @@
 import basic.BubbleSort;
+import basic.InsertionSort;
 import basic.SelectionSort;
 import common.SortList;
 
@@ -7,18 +8,24 @@ public class Main {
 
         Integer[] list = {8, 31, 48, 73, 3, 65, 20, 29, 11, 5};
 
-        SortList<Integer> selectionSort = new BubbleSort<>(list);
-        selectionSort.sort();
-        selectionSort.print();
+        // SortList<Integer> sort = new SelectionSort<>(list);
+        // SortList<Integer> sort = new BubbleSort<>(list);
+        SortList<Integer> sort = new InsertionSort<>(list);
+        sort.sort();
+        sort.print();
 
         String[] stringList = {"가", "자", "다", "나", "라", "파"};
-        SortList<String> stringSelectionSort = new BubbleSort<>(stringList);
-        stringSelectionSort.sort();
-        stringSelectionSort.print();
+        // SortList<String> stringSort = new SelectionSort<>(stringList);
+        // SortList<String> stringSort = new BubbleSort<>(stringList);
+        SortList<String> stringSort = new InsertionSort<>(stringList);
+        stringSort.sort();
+        stringSort.print();
 
         Double[] doubleList = {8.0, 31.0, 48.3, 48.2, 47.9, 20.1, 29.3, 11.7, 5.5};
-        SortList<Double> doubleSelectionSort = new BubbleSort<>(doubleList);
-        doubleSelectionSort.sort();
-        doubleSelectionSort.print();
+        // SortList<Double> doubleSort = new SelectionSort<>(doubleList);
+        // SortList<Double> doubleSort = new BubbleSort<>(doubleList);
+        SortList<Double> doubleSort = new InsertionSort<>(doubleList);
+        doubleSort.sort();
+        doubleSort.print();
     }
 }
