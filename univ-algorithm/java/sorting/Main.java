bd221aac125ee58ef2a7f39833ca4e4c4ec3341b
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2024-09-19 16:44:00 +0900

    Merge Sort

--- univ-algorithm/java/sorting/Main.java
+++ univ-algorithm/java/sorting/Main.java
@@ -1,3 +1,4 @@
+import advanced.MergeSort;
 import basic.BubbleSort;
 import basic.InsertionSort;
 import basic.SelectionSort;
@@ -10,21 +11,24 @@ public class Main {
 
         // SortList<Integer> sort = new SelectionSort<>(list);
         // SortList<Integer> sort = new BubbleSort<>(list);
-        SortList<Integer> sort = new InsertionSort<>(list);
+        // SortList<Integer> sort = new InsertionSort<>(list);
+        SortList<Integer> sort = new MergeSort<>(list);
         sort.sort();
         sort.print();
 
         String[] stringList = {"가", "자", "다", "나", "라", "파"};
         // SortList<String> stringSort = new SelectionSort<>(stringList);
         // SortList<String> stringSort = new BubbleSort<>(stringList);
-        SortList<String> stringSort = new InsertionSort<>(stringList);
+        // SortList<String> stringSort = new InsertionSort<>(stringList);
+        SortList<String> stringSort = new MergeSort<>(stringList);
         stringSort.sort();
         stringSort.print();
 
         Double[] doubleList = {8.0, 31.0, 48.3, 48.2, 47.9, 20.1, 29.3, 11.7, 5.5};
         // SortList<Double> doubleSort = new SelectionSort<>(doubleList);
         // SortList<Double> doubleSort = new BubbleSort<>(doubleList);
-        SortList<Double> doubleSort = new InsertionSort<>(doubleList);
+        // SortList<Double> doubleSort = new InsertionSort<>(doubleList);
+        SortList<Double> doubleSort = new MergeSort<>(doubleList);
         doubleSort.sort();
         doubleSort.print();
     }
